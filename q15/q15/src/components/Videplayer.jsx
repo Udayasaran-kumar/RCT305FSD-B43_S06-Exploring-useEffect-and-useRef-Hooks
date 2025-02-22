dc8e1b6bf22e52ca15c88f5a3bb828a03496ab9c
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-02-22 23:05:01 +0530

    q15 added

---
+++ q15/q15/src/components/Videplayer.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useRef } from "react";
+
+function Videoplayer() {
+  const [time, setTime] = useState(0);
+  const intervalId = useRef(null);
+
+  const startTimer = () => {
+    if (intervalId.current !== null) return;
+    intervalId.current = setInterval(() => {
+      setTime((prevTime) =>  prevTime + 1);
+    }, 1000);
+  };
+
+  const stopTimer = () => {    
+    clearInterval(intervalId.current);  
+    intervalId.current = null;     
+  };
+
+  const resetTimer = () => {
+    clearInterval(intervalId.current); 
+    intervalId.current=null;
+    setTime(0);
+  };
+
+  return (
+    <div>
+      <h1>video: {time}</h1>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Pause</button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
+}
+
+export default Videoplayer;
